3057708b504afdb8bf0036518c6ab10cf97fe85f
Ethan Huang 黃靖緯 <69299174+chingweih@users.noreply.github.com>
2024-12-07 22:21:15 +0800

    finish my portfolio landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
+import colors from 'tailwindcss/colors'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      ...defaultTheme,
+      fontFamily: {
+        sans: ['Noto Sans TC Variable', ...defaultTheme.fontFamily.sans],
+        serif: ['Noto Serif TC Variable', ...defaultTheme.fontFamily.serif],
+      },
+    },
     colors: {
+      ...colors,
       bg: '#fafafa',
     },
   },
