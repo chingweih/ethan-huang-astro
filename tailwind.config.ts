f91ff815ed2653fc97ce29de567adc23de0389c8
Ethan Huang 黃靖緯 <69299174+chingweih@users.noreply.github.com>
2024-12-08 17:19:58 +0800

    fix tailwind config (custom colors should be put in `extend` in order to not break default theme)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,18 @@
-import colors from 'tailwindcss/colors'
 import defaultTheme from 'tailwindcss/defaultTheme'
+import { type Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      ...defaultTheme,
       fontFamily: {
         sans: ['Noto Sans TC Variable', ...defaultTheme.fontFamily.sans],
         serif: ['Noto Serif TC Variable', ...defaultTheme.fontFamily.serif],
       },
-    },
-    colors: {
-      ...colors,
-      bg: '#fafafa',
+      colors: {
+        bg: '#fafafa',
+      },
     },
   },
   plugins: [],
-}
+} satisfies Config
